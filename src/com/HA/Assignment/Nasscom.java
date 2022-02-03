999257853e512c83dd09db17497a76fa83584d4f
SowmyaReddy2000 <21sowmya2000@gmail.com>
2022-02-03 19:02:14 +0530

    Java1

--- src/com/HA/Assignment/Nasscom.java
+++ src/com/HA/Assignment/Nasscom.java
@@ -3,8 +3,24 @@ package com.HA.Assignment;
 public class Nasscom {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
+
+
+/*    1. Navigate onto https://nasscom.in/
+2. Click on New User 
+driver.findElement(By.partialLinkText("New User")).click();
+
+3.  Enter First name as admin
+4. Enter Last name as pass
+5. Enter email address as admin@gmail.com
+6. Enter company name as
+7.  Google
+8. Select IT Consulting from dropdown 
+9. No need to automate CAPTCHA
+10. Click on Register 
+*/
